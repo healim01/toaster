22a27acacc86529c5330097c8ecdc137a165695d
Hyelim Choi <HyelimChoi01@gmail.com>
2025-03-26 18:41:06 +0900

    chore: eslint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
