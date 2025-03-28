f0820b26c22a2396545a2047d40a852ec5e7fce8
Hyelim Choi <HyelimChoi01@gmail.com>
2025-03-29 00:57:43 +0900

    feat: 찍은 사진 지우고 홈으로 돌아가기

--- src/pages/PreviewPage.tsx
+++ src/pages/PreviewPage.tsx
@@ -1,8 +1,21 @@
+import { Button } from '@/components';
 import { FrameSelectSection, PhotoFrame } from '@/components/preview';
+import { ROUTE_PATH } from '@/constants/routePath';
+import { usePhotosContext } from '@/hooks';
+import { useNavigate } from 'react-router-dom';
 
 const PreviewPage = () => {
+  const navigate = useNavigate();
+  const { setPhotos } = usePhotosContext();
+
+  const handleReturn = () => {
+    setPhotos([]);
+    navigate(ROUTE_PATH.home);
+  };
+
   return (
     <div className="flex flex-row items-center justify-center w-full gap-10 bg-amber-50">
+      <Button label="home" onClick={handleReturn} />
       <PhotoFrame />
       <FrameSelectSection />
     </div>
