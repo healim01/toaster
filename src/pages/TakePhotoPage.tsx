9e50403edd0e05752956b5f87cb7f3c8c8a66ece
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-03 16:49:13 +0900

    feat: 4컷 촬영 이후 페이지 이동

--- src/pages/TakePhotoPage.tsx
+++ src/pages/TakePhotoPage.tsx
@@ -3,8 +3,19 @@ import {
   CameraFrame,
   FilterSelectSection,
 } from '@/components/takePhoto';
+import { ROUTE_PATH } from '@/constants/routePath';
+import { usePhotosContext } from '@/hooks';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const TakePhotoPage = () => {
+  const navigate = useNavigate();
+  const { photos } = usePhotosContext();
+
+  useEffect(() => {
+    if (photos.length >= 4) navigate(ROUTE_PATH.preview);
+  }, [photos]);
+
   return (
     <div className="flex flex-row items-center justify-center w-screen h-screen gap-10 bg-amber-50">
       <div className="flex flex-col">
