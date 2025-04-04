1390b68e0053258714ffd7a46efff77783b07698
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-04 17:01:19 +0900

    chore: 3D 아이콘 이미지 추가

--- src/assets/imgs/index.ts
+++ src/assets/imgs/index.ts
@@ -1,2 +1,4 @@
+export { default as basketImg } from './basket.png';
+export { default as strawberryJamImg } from './strawberry-jam.png';
 export { default as toastImg } from './toast.png';
 export { default as toasterImg } from './toaster.png';
