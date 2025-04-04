a5522eb56b05862263bd4dfac94d42328aabbbcb
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-04 20:31:00 +0900

    chore: jam 에셋 추가

--- src/assets/imgs/index.ts
+++ src/assets/imgs/index.ts
@@ -1,4 +1,9 @@
 export { default as basketImg } from './basket.png';
-export { default as strawberryJamImg } from './strawberry-jam.png';
 export { default as toastImg } from './toast.png';
 export { default as toasterImg } from './toaster.png';
+
+// jam
+export { default as blueberryJamImg } from './jam/blueberry-jam.png';
+export { default as peachJamImg } from './jam/peach-jam.png';
+export { default as peanutButterJamImg } from './jam/peanutButter-jam.png';
+export { default as strawberryJamImg } from './jam/strawberry-jam.png';
