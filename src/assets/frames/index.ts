2d9f4982933c8362b67089125c312b2ee312d453
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-05 19:21:12 +0900

    chore: 프레임 추가

--- src/assets/frames/index.ts
+++ src/assets/frames/index.ts
@@ -1,6 +1,8 @@
 export { default as blueFrame } from './blue.png';
+export { default as butterFrame } from './butter.png';
 export { default as frame } from './frame.png';
 export { default as gdyFrame } from './gdyFrame.png';
 export { default as grassFrame } from './grass.png';
 export { default as greenFrame } from './greenFrame.png';
-export { default as pinkGreenFrame } from './pinkGreen.png';
+export { default as pinkFrame } from './pink.png';
+export { default as whiteFrame } from './white.png';
--- src/constants/frames.ts
+++ src/constants/frames.ts
@@ -1,17 +1,21 @@
 import {
   blueFrame,
+  butterFrame,
   frame,
   gdyFrame,
   grassFrame,
   greenFrame,
-  pinkGreenFrame,
+  pinkFrame,
+  whiteFrame,
 } from '@/assets/frames';
 
 export const frames: Record<string, string> = {
   default: frame,
   버건디: gdyFrame,
   무한도전: greenFrame,
-  핑크그린: pinkGreenFrame,
   초록초록: grassFrame,
   블루베리: blueFrame,
+  핑크: pinkFrame,
+  버터: butterFrame,
+  화이트: whiteFrame,
 };
