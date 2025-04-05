8800e354b3ca0de7ce605afdbea3b4b9e8e87789
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-05 19:23:18 +0900

    feat: 보정 필터 추가

--- src/constants/filter.ts
+++ src/constants/filter.ts
@@ -15,24 +15,55 @@ export const Filter: Record<string, FilterObject> = {
     iconUrl: strawberryJamImg,
     openIconUrl: strawberryJamOpenImg,
     name: '딸기잼',
-    filterStyle: 'grayscale(20%) brightness(1.1) contrast(110%) blur(1px)',
+    filterStyle: `grayscale(20%) 
+    brightness(1.1) 
+    contrast(110%) 
+    blur(.5px)`,
   },
   peach: {
     iconUrl: peachJamImg,
     openIconUrl: peachJamOpenImg,
     name: '복숭아잼',
-    filterStyle: 'grayscale(20%) brightness(1.1) contrast(110%) blur(1px)',
+    filterStyle: `grayscale(30%) 
+    brightness(1.1) 
+    contrast(110%)`,
   },
   peanut: {
     iconUrl: peanutButterJamImg,
     openIconUrl: peanutButterJamOpenImg,
     name: '땅콩잼',
-    filterStyle: 'grayscale(20%) brightness(1.1) contrast(110%) blur(1px)',
+    filterStyle: `grayscale(20%) 
+    brightness(1.1) 
+    contrast(1.1) 
+    saturate(1.2) 
+    blur(.5px) 
+    hue-rotate(5deg)`,
   },
   blueberry: {
     iconUrl: blueberryJamImg,
     openIconUrl: blueberryJamOpenImg,
     name: '블루베리잼',
-    filterStyle: 'grayscale(20%) brightness(1.1) contrast(110%) blur(1px)',
+    filterStyle: `grayscale(20%) 
+      brightness(1.1) 
+      contrast(110%) 
+      blur(1px)`,
+  },
+  fresh: {
+    iconUrl: blueberryJamImg,
+    openIconUrl: blueberryJamOpenImg,
+    name: '프레쉬',
+    filterStyle: `brightness(1.2)
+      contrast(1)
+      saturate(1.1)
+      sepia(0.05)
+      drop-shadow(0 0 5px rgba(255,255,255,0.5))`,
+  },
+  black: {
+    iconUrl: blueberryJamImg,
+    openIconUrl: blueberryJamOpenImg,
+    name: 'black',
+    filterStyle: `grayscale(100%) 
+      contrast(150%) 
+      brightness(110%)`,
   },
 };
