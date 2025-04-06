2767e92331c0138d0d94e8f1f811f00da9de93f4
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-06 17:24:35 +0900

    chore: 헤더 레이아웃 위치 조정

--- src/pages/CustomPhotoPage.tsx
+++ src/pages/CustomPhotoPage.tsx
@@ -13,8 +13,8 @@ const CustomPhotoPage = () => {
   };
 
   return (
-    <div className="flex relative w-full h-full pt-2 pb-2">
-      <div className="flex flex-col md:flex-row gap-4 w-full h-full">
+    <div className="flex relative w-full h-[92%] pt-2 pb-2">
+      <div className="flex flex-col  md:flex-row gap-4 w-full h-full mt-[50px]">
         {/* 사진 영역 */}
         <div className="flex items-center justify-center w-full md:w-[280px] h-full bg-gray-50 p-4 rounded-lg shadow-md">
           <PhotoFrame />
