c6197a62c2ae378097dd5efc382bd200a0665cff
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-06 15:17:16 +0900

    chore: location 에 따른 배경색 변경

--- src/components/_common/Layout/Layout.tsx
+++ src/components/_common/Layout/Layout.tsx
@@ -1,8 +1,18 @@
+import { ROUTE_PATH } from '@/constants/routePath';
 import { PropsWithChildren } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Layout = ({ children }: PropsWithChildren) => {
+  const location = useLocation();
+
   return (
-    <div className="flex flex-col w-screen h-screen bg-blue-200">
+    <div
+      className={`flex flex-col w-screen h-screen ${
+        location.pathname === ROUTE_PATH.takePhoto
+          ? 'bg-amber-100'
+          : 'bg-blue-200'
+      }`}
+    >
       {children}
     </div>
   );
