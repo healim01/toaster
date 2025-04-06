a1b28851d158ca1d118ab4e9395909d7e0b507d8
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-06 20:58:47 +0900

    chore: marquee keyframes 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { FilterProvider, FrameProvider, PhotosProvider } from '@/context';
 import router from '@/router';
 import '@/styles/global.css';
+import '@/styles/tailwind.css';
 import '@/styles/theme.css';
 import { RouterProvider } from 'react-router-dom';
 
