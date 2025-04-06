ce271b22b1aec12e54a77897be47e9bc1d5670ae
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-06 16:56:56 +0900

    feat: 로고 아이콘 추가

--- src/components/_common/Header/Header.tsx
+++ src/components/_common/Header/Header.tsx
@@ -1,6 +1,5 @@
-import { Button } from '@/components';
+import { TextToasterIcon } from '@/assets';
 import { ROUTE_PATH } from '@/constants/routePath';
-import { HeaderSize } from '@/constants/system';
 import { usePhotosContext } from '@/hooks';
 import { useNavigate } from 'react-router-dom';
 
@@ -15,10 +14,10 @@ const Header = () => {
 
   return (
     <div
-      className={`sticky top-0 flex items-center w-full h-[${HeaderSize}px] bg-white z-10 shrink-0`}
+      className={`sticky top-0 flex items-center w-full h-[45px] z-10 shrink-0`}
     >
-      <div className="flex flex-row w-full h-full max-w-screen-xl px-4 sm:px-6 md:px-8 mx-auto">
-        <Button label="toaster" onClick={handleReturn} />
+      <div className="flex flex-row items-center w-full h-full max-w-screen-xl px-4 sm:px-6 md:px-8 mx-auto">
+        <TextToasterIcon width={150} height={40} onClick={handleReturn} />
       </div>
     </div>
   );
