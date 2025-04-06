80b7e87e8c10e64ff5e73623c82be0d44c6244f3
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-06 17:32:10 +0900

    chore: 배경색 변경

--- src/components/_common/Layout/Layout.tsx
+++ src/components/_common/Layout/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: PropsWithChildren) => {
       className={`flex flex-col w-screen m-h-screen ${
         location.pathname === ROUTE_PATH.takePhoto
           ? 'bg-amber-50'
-          : 'bg-blue-200'
+          : 'bg-blue-100'
       }`}
     >
       {children}
--- src/components/_common/Layout/MainLayout.tsx
+++ src/components/_common/Layout/MainLayout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 const MainLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="w-full h-screen mx-auto px-4 sm:px-6 md:px-8 max-w-screen-xl overflow-y-scroll bg-pink-50">
+    <div className="w-full h-screen mx-auto px-4 sm:px-6 md:px-8 max-w-screen-xl overflow-y-scroll">
       <div className="flex flex-col w-full h-full">{children}</div>
     </div>
   );
