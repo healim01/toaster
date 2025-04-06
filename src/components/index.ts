261c739218e499a23c3efaccc3b017898ee3b6d9
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-06 21:00:11 +0900

    feat: marquee 컴포넌트 제작

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Dropdown } from './_common/Dropdown/Dropdown';
 export { default as Header } from './_common/Header/Header';
 export { default as Layout } from './_common/Layout/Layout';
 export { default as MainLayout } from './_common/Layout/MainLayout';
+export { default as Marquee } from './_common/Marquee/Marquee';
