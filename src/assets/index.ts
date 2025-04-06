799ab502ce1c294b55f474cb064abf0e5ff4ed95
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-06 18:50:26 +0900

    chore: sns icon assets 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,7 @@
 export { default as toastImg } from './imgs/toast.png';
 
 // icons
+export { default as GithubIcon } from './icons/github.svg?react';
+export { default as InstagramIcon } from './icons/instagram.svg?react';
 export { default as TextToasterIcon } from './icons/toaster-text.svg?react';
 export { default as ToasterIcon } from './icons/toaster.svg?react';
