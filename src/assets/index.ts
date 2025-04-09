2b3411a2a82976184bc79d3f1fc22da99bd169a2
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-09 23:09:38 +0900

    chore: 이미지 에셋 변경

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,26 @@
-// imgs
-export { default as toastImg } from './imgs/toast.png';
-
 // icons
 export { default as GithubIcon } from './icons/github.svg?react';
 export { default as InstagramIcon } from './icons/instagram.svg?react';
 export { default as TextToasterIcon } from './icons/toaster-text.svg?react';
 export { default as ToasterIcon } from './icons/toaster.svg?react';
+
+// imgs
+export { default as photoaster } from './imgs/photoaster.png';
+export { default as toasterBoothImg } from './imgs/toasterBooth.png';
+
+// toast
+export { default as avocadoToastImg } from './imgs/toast/avocado-toast.png';
+export { default as blueberryToastImg } from './imgs/toast/blueberry-toast.png';
+export { default as eggToastImg } from './imgs/toast/egg-toast.png';
+export { default as strawberryToastImg } from './imgs/toast/strawberry-toast.png';
+export { default as toastImg } from './imgs/toast/toast.png';
+
+// jam
+export { default as blueberryJamOpenImg } from './imgs/jam/blueberry-jam-open.png';
+export { default as blueberryJamImg } from './imgs/jam/blueberry-jam.png';
+export { default as peachJamOpenImg } from './imgs/jam/peach-jam-open.png';
+export { default as peachJamImg } from './imgs/jam/peach-jam.png';
+export { default as peanutButterJamOpenImg } from './imgs/jam/peanutbutter-jam-open.png';
+export { default as peanutButterJamImg } from './imgs/jam/peanutbutter-jam.png';
+export { default as strawberryJamOpenImg } from './imgs/jam/strawberry-jam-open.png';
+export { default as strawberryJamImg } from './imgs/jam/strawberry-jam.png';
