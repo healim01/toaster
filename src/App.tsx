c3d660ec3d54859f5b22b4b3e4c07127d212bb55
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-11 16:46:18 +0900

    chore: amplitude 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { FilterProvider, FrameProvider, PhotosProvider } from '@/context';
 import router from '@/router';
+import { amplitudeInitializer } from '@/service/amplitude/amplitudeInitializer';
 import '@/styles/global.css';
 import '@/styles/tailwind.css';
 import '@/styles/theme.css';
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
+const { init } = amplitudeInitializer();
+
 function App() {
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
     <FilterProvider>
       <PhotosProvider>
