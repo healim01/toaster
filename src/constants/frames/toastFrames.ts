646f7b658a0e5d5a4cf5b4255b3dd2e77f91867b
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-13 19:54:57 +0900

    chore: 진짜 토스트 프레임 추가

--- src/constants/frames/toastFrames.ts
+++ src/constants/frames/toastFrames.ts
@@ -1,4 +1,5 @@
 import {
+  realToastFrame,
   toast2Frame,
   toast3D2Frame,
   toast3DFrame,
@@ -6,7 +7,8 @@ import {
 } from '@/assets/frames';
 
 export const toastFrames: Record<string, string> = {
-  토스트: toastFrame,
+  토스트: realToastFrame,
+  버터토스트: toastFrame,
   토스트ed: toast2Frame,
   '딸기잼 묻은 토스트': toast3DFrame,
   '도리도리 토스트': toast3D2Frame,
