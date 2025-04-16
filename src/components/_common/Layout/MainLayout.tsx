554f230dc6ec7788c99cca493e5a880cc8931a98
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-17 03:00:12 +0900

    chore: 모바일 기능 블록커 삭제

--- src/components/_common/Layout/MainLayout.tsx
+++ src/components/_common/Layout/MainLayout.tsx
@@ -1,11 +1,6 @@
-import { MobileBlocker } from '@/components/etc';
-import { useIsMobileDevice } from '@/hooks';
 import { PropsWithChildren } from 'react';
 
 const MainLayout = ({ children }: PropsWithChildren) => {
-  const isMobile = useIsMobileDevice();
-  if (isMobile) return <MobileBlocker />;
-
   return (
     <div className="w-full min-h-screen h-full mx-auto px-4 sm:px-6 md:px-8 max-w-screen-xl mt-2">
       <div className="flex flex-col w-full h-full">{children}</div>
