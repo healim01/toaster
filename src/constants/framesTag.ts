be8ed2cb1fb439224c94b7d3ebdc4e231a66c88e
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-16 15:55:13 +0900

    feat: 프레임 디스플레이 정렬 순서 로직 추가

--- src/constants/framesTag.ts
+++ src/constants/framesTag.ts
@@ -1,1 +1,2 @@
 export const DEFAULT_FRAMES_TAGS = ['default'];
+export const PRIORITY_FRAMES_TAGS = ['events', 'toast', 'basic'];
--- src/service/supabase/getFrames.ts
+++ src/service/supabase/getFrames.ts
@@ -1,3 +1,4 @@
+import { PRIORITY_FRAMES_TAGS } from '@/constants/framesTag';
 import { getStoragePublicUrl, supabaseClient } from '@/service/supabase';
 
 const getFrames = async (tags: string[]) => {
@@ -5,14 +6,23 @@ const getFrames = async (tags: string[]) => {
     .from('frames')
     .select('*')
     .overlaps('tags', tags)
-    .eq('is_active', true);
+    .eq('is_active', true)
+    .order('created_at', { ascending: true });
 
   if (error) throw error;
 
-  return data.map(frame => ({
-    ...frame,
-    url: getStoragePublicUrl(frame.image_path),
-  }));
+  return data
+    .map(frame => {
+      const index = PRIORITY_FRAMES_TAGS.findIndex(priorityTag =>
+        frame.tags.includes(priorityTag),
+      );
+      return {
+        ...frame,
+        url: getStoragePublicUrl(frame.image_path),
+        _priorityIndex: index === -1 ? Infinity : index,
+      };
+    })
+    .sort((a, b) => a._priorityIndex - b._priorityIndex);
 };
 
 export default getFrames;
