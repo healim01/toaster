60e727b5726dde4598503a3e775561eb49d07358
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-16 18:23:20 +0900

    chore: 사용하지 않는 파일 삭제

--- src/service/supabase/index.ts
+++ src/service/supabase/index.ts
@@ -1,4 +1,3 @@
-export { default as getFrame } from './getFrame';
 export { default as getFrames } from './getFrames';
 export { default as getStoragePublicUrl } from './getStoragePublicUrl';
 export { default as supabaseClient } from './supabaseClient';
