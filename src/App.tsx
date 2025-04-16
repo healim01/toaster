df441e1bd34aae28694ecc7b894657db824d0704
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-16 16:30:18 +0900

    chore: 상태관리를 위해 tanstack 라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { amplitudeInitializer } from '@/service/amplitude/amplitudeInitializer';
 import '@/styles/global.css';
 import '@/styles/tailwind.css';
 import '@/styles/theme.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
 const { init } = amplitudeInitializer();
+const queryClient = new QueryClient();
 
 function App() {
   useEffect(() => {
@@ -15,13 +17,15 @@ function App() {
   }, []);
 
   return (
-    <FilterProvider>
-      <PhotosProvider>
-        <FrameProvider>
-          <RouterProvider router={router} />
-        </FrameProvider>
-      </PhotosProvider>
-    </FilterProvider>
+    <QueryClientProvider client={queryClient}>
+      <FilterProvider>
+        <PhotosProvider>
+          <FrameProvider>
+            <RouterProvider router={router} />
+          </FrameProvider>
+        </PhotosProvider>
+      </FilterProvider>
+    </QueryClientProvider>
   );
 }
 
