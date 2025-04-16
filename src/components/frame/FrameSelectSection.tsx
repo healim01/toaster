d472929ada154e6bb47f567f5c7e52261edcef9c
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-16 15:42:54 +0900

    refactor: useFrames, useGetFramesByEvent 훅 로직 병합

--- src/components/frame/FrameSelectSection.tsx
+++ src/components/frame/FrameSelectSection.tsx
@@ -1,14 +1,14 @@
 import { FrameButton } from '@/components/frame';
-import useFrames from '@/hooks/useFrames';
+import { useGetFramesByEvent } from '@/hooks';
 
 const FrameSelectSection = () => {
-  const { frames } = useFrames();
+  const frames = useGetFramesByEvent();
 
   return (
     <section className="flex flex-col w-full h-fit md:h-[85%] p-5 bg-gray-50 rounded-lg md:overflow-y-scroll shadow-md">
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4 w-full justify-center justify-items-center mb-4">
-        {Object.entries(frames).map(([name, url]) => (
-          <FrameButton key={name} frameName={name} frameUrl={url} />
+        {frames.frames.map(frame => (
+          <FrameButton key={frame.name} frame={frame} />
         ))}
       </div>
     </section>
--- src/hooks/useGetFramesByEvent.ts
+++ src/hooks/useGetFramesByEvent.ts
@@ -1,25 +1,26 @@
 import { DEFAULT_FRAMES_TAGS } from '@/constants/framesTag';
 import { getFrames } from '@/service/supabase';
+import { Frame } from '@/types/frame';
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
-type Frame = {
-  id: string;
-  name: string;
-  url: string;
-  tags: string[];
-};
-
-const useGetFramesByEvent = (event?: string) => {
+const useGetFramesByEvent = () => {
   const [frames, setFrames] = useState<Frame[]>([]);
+
+  const query = new URLSearchParams(useLocation().search);
+  const eventId = query.get('eventId') ?? undefined;
 
   useEffect(() => {
     const getData = async () => {
-      const data = await getFrames([...DEFAULT_FRAMES_TAGS, event ?? '']);
+      const data = await getFrames([
+        ...DEFAULT_FRAMES_TAGS,
+        ...(eventId ? [eventId] : []),
+      ]);
       setFrames(data);
     };
 
     getData();
-  }, [event]);
+  }, [eventId]);
 
   return { frames };
 };
