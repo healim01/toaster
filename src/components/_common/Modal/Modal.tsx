92104d001d0cd6f5dc18c59783784d0c1419228d
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-18 01:21:18 +0900

    feat: 공용 모달 컴포넌트 제작

---
+++ src/components/_common/Modal/Modal.tsx
@@ -0,0 +1,46 @@
+import { CloseIcon } from '@/assets';
+import { ReactNode, useEffect } from 'react';
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+const Modal = ({ isOpen, onClose, children }: Props) => {
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') onClose();
+    };
+    document.addEventListener('keydown', handleEsc);
+    return () => document.removeEventListener('keydown', handleEsc);
+  }, [onClose]);
+
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-gray-300/50"
+      onClick={onClose}
+    >
+      <div
+        className="relative bg-white rounded-xl p-6 shadow-lg w-full max-w-md mx-4"
+        onClick={e => e.stopPropagation()}
+      >
+        <button
+          className="absolute top-4 right-4
+             bg-white border border-gray-300 rounded-full 
+             w-10 aspect-square flex items-center justify-center 
+             shadow-lg hover:shadow-xl transition"
+          onClick={onClose}
+          aria-label="Close"
+        >
+          <CloseIcon />
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
