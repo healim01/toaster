1252a52a8a3eef164768a6f95cd19da58d330f13
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-18 01:53:54 +0900

    feat: 로컬 스토리지 사용해서 봤던 공지 안띄우게 작업

--- src/components/Notice/NoticeModalSection.tsx
+++ src/components/Notice/NoticeModalSection.tsx
@@ -1,14 +1,28 @@
 import { Modal } from '@/components';
 import { useGetNoticeQuery } from '@/hooks/queries';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const NoticeModalSection = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   const { notice } = useGetNoticeQuery();
+
+  useEffect(() => {
+    const isNoticeViewed = localStorage.getItem(
+      `noticeViewed ${notice?.title}`,
+    );
+    if (!isNoticeViewed && notice) {
+      setIsOpen(true);
+    }
+  }, [notice]);
+
+  const handleClose = () => {
+    localStorage.setItem(`noticeViewed ${notice?.title}`, 'true');
+    setIsOpen(false);
+  };
 
   return (
     notice && (
-      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+      <Modal isOpen={isOpen} onClose={handleClose}>
         <div className="w-[300px] md:w-[400px] gap-5 flex flex-col items-center">
           <div className="w-full h-[300px] md:h-[400px]">
             <img src={notice.url} className="w-full h-full" />
