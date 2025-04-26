f1603f232112cbab06ef8781646a76bb789b137c
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-26 16:15:56 +0900

    feat: 구글 로그인 기능 제작

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,22 +1,32 @@
 import { Button } from '@/components';
+import useUserContext from '@/hooks/useUserContext';
 import { supabaseClient } from '@/service/supabase';
+
 const LoginButton = () => {
   const handleLogin = async () => {
-    const { data, error } = await supabaseClient.auth.signInWithOAuth({
+    await supabaseClient.auth.signInWithOAuth({
       provider: 'google',
       options: {
         redirectTo: `${window.location.origin}/login`,
       },
     });
-    console.log(data, error);
   };
 
   return <Button label="구글로 로그인" onClick={handleLogin} />;
 };
+
 const LoginPage = () => {
+  const { user } = useUserContext();
+
   return (
     <div className="flex flex-row flex-wrap items-center justify-center w-full h-screen">
-      <LoginButton />
+      {user ? (
+        <p>
+          반가워요, {user.name}님! ({user.email})
+        </p>
+      ) : (
+        <LoginButton />
+      )}
     </div>
   );
 };
