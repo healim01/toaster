1a80681363cd5712368188c2a894da183bfe8bd8
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-28 21:55:58 +0900

    feat: 사진 로드 전 스켈레톤 처리로 layout shift 막기

--- src/components/gallery/GalleryItem.tsx
+++ src/components/gallery/GalleryItem.tsx
@@ -1,14 +1,14 @@
-import { GalleryModal } from '@/components/gallery/GalleryModal';
+import { GalleryModal } from '@/components/gallery';
 import { Photo } from '@/types/photo';
 import { getFormatDate } from '@/utils/getFormatDate';
 import { useState } from 'react';
 
-interface Props {
-  photo: Photo;
-}
-export const GalleryItem = ({ photo }: Props) => {
+export const GalleryItem = ({ photo }: { photo: Photo }) => {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+
   const handleToggleModal = () => {
+    if (!isImageLoaded) return;
     setIsOpen(prev => !prev);
   };
 
@@ -18,19 +18,35 @@ export const GalleryItem = ({ photo }: Props) => {
         onClick={handleToggleModal}
         className="w-fit p-4 rounded-xl overflow-hidden shadow-md bg-white hover:shadow-lg transition-shadow duration-300 cursor-pointer"
       >
+        {!isImageLoaded && (
+          <div className="w-[180px] h-[500px] bg-gray-200 animate-pulse rounded-xl" />
+        )}
+
         <img
           src={photo.url}
           alt="user photo"
-          className="w-[180px] h-fit object-cover"
+          className={`w-[180px] h-fit object-cover ${
+            isImageLoaded ? 'block' : 'hidden'
+          }`}
+          onLoad={() => setIsImageLoaded(true)}
         />
-        <div className="pt-2">
-          <div className="text-sm text-gray-500 text-right">
-            {getFormatDate(photo.created_at)}
+
+        {isImageLoaded && (
+          <div className="pt-2">
+            <div className="text-sm text-gray-500 text-right">
+              {getFormatDate(photo.created_at)}
+            </div>
           </div>
-        </div>
+        )}
       </div>
 
-      <GalleryModal isOpen={isOpen} onClose={handleToggleModal} photo={photo} />
+      {isImageLoaded && (
+        <GalleryModal
+          isOpen={isOpen}
+          onClose={handleToggleModal}
+          photo={photo}
+        />
+      )}
     </>
   );
 };
