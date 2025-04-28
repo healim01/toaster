481bc7033d17ca083f5274782bf627acb3878c1f
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-28 21:32:07 +0900

    feat: 업로드 후 저장 페이지에서 토스트 메세지 전달

--- src/components/photo/PhotoDownloadSection.tsx
+++ src/components/photo/PhotoDownloadSection.tsx
@@ -1,25 +1,36 @@
-import { Button } from '@/components';
+import { Button, ToastMessage } from '@/components';
 import { PhotoFrame } from '@/components/photo';
 import { usePhotoDownload } from '@/hooks';
 import { usePhotoUpload } from '@/hooks/usePhotoUpload';
 import { useEffect, useRef, useState } from 'react';
 
 const PhotoDownloadSection = () => {
+  const [showToast, setShowToast] = useState(false);
   const downloadDivRef = useRef<HTMLDivElement>(null);
   const { handleDownload, isLoading } = usePhotoDownload(downloadDivRef);
   const { handleUpload, isSuccess } = usePhotoUpload(downloadDivRef);
 
   useEffect(() => {
     handleUpload();
-    if (isSuccess) alert('사진 업로드 성공');
   }, []);
 
+  useEffect(() => {
+    if (isSuccess) {
+      setShowToast(true);
+      setTimeout(() => setShowToast(false), 3000);
+    }
+  }, [isSuccess]);
+
   return (
-    <div className="flex flex-col items-end w-fit h-fit md:flex-row p-5 gap-5 bg-white rounded-2xl">
-      {/* 사진 프레임 */}
-      <div ref={downloadDivRef}>
-        <PhotoFrame />
-      </div>
+    <>
+      {showToast && (
+        <ToastMessage message="사진 업로드에 성공했습니다! 이제 내 갤러리에서 다시 볼 수 있어요!" />
+      )}
+      <div className="flex flex-col items-end w-fit h-fit md:flex-row p-5 gap-5 bg-white rounded-2xl">
+        {/* 사진 프레임 */}
+        <div ref={downloadDivRef}>
+          <PhotoFrame />
+        </div>
 
         {/* 다운로드 버튼 */}
         <Button
