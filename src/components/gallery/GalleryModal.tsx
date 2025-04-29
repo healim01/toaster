c23c25a1ec90ac6e6a04b3f59fb73fa519d0aaff
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-29 12:02:46 +0900

    feat: 사진 다운로드 실패시 에러 처리

--- src/components/gallery/GalleryModal.tsx
+++ src/components/gallery/GalleryModal.tsx
@@ -1,7 +1,8 @@
-import { Button, Modal } from '@/components';
+import { Button, Modal, ToastMessage } from '@/components';
 import { Photo } from '@/types/photo';
 import { downloadImageFromUrl } from '@/utils/downloadImageFromUrl';
 import { getFormatDate } from '@/utils/getFormatDate';
+import { useState } from 'react';
 
 interface Props {
   isOpen: boolean;
@@ -10,29 +11,47 @@ interface Props {
 }
 
 export const GalleryModal = ({ isOpen, onClose, photo }: Props) => {
+  const [showToast, setShowToast] = useState(false);
+
   const handleDownload = async () => {
-    await downloadImageFromUrl(photo.url, `${photo.name}`);
+    try {
+      await downloadImageFromUrl(photo.url, `${photo.name}`);
+    } catch (error) {
+      console.error('Failed to download the image:', error);
+
+      setShowToast(true);
+      const timeoutId = setTimeout(() => setShowToast(false), 3000);
+      return () => clearTimeout(timeoutId);
+    }
   };
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <div className="flex gap-4">
-        <div className="flex flex-col">
-          <img
-            src={photo.url}
-            alt="user photo"
-            className="w-[180px] h-fit object-cover"
-          />
-          <div className="pt-2">
-            <div className="text-sm text-gray-500 text-right">
-              {getFormatDate(photo.created_at)}
+    <>
+      {showToast && (
+        <ToastMessage
+          type="error"
+          message="사진 다운로드에 실패했습니다. 다시 시도해주세요"
+        />
+      )}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <div className="flex gap-4">
+          <div className="flex flex-col">
+            <img
+              src={photo.url}
+              alt="user photo"
+              className="w-[180px] h-fit object-cover"
+            />
+            <div className="pt-2">
+              <div className="text-sm text-gray-500 text-right">
+                {getFormatDate(photo.created_at)}
+              </div>
             </div>
           </div>
+          <div className="flex flex-col-reverse">
+            <Button label="다운받기" onClick={handleDownload} size="medium" />
+          </div>
         </div>
-        <div className="flex flex-col-reverse">
-          <Button label="다운받기" onClick={handleDownload} size="medium" />
-        </div>
-      </div>
-    </Modal>
+      </Modal>
+    </>
   );
 };
