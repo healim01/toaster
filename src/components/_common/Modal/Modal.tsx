ef9c650995590304e9ab4b3c44ad870693b2866b
Hyelim Choi <HyelimChoi01@gmail.com>
2025-05-03 19:20:45 +0900

    chore: 기존 overflow 스타일 override 하기

--- src/components/_common/Modal/Modal.tsx
+++ src/components/_common/Modal/Modal.tsx
@@ -17,14 +17,15 @@ const Modal = ({ isOpen, onClose, children }: Props) => {
   }, [onClose]);
 
   useEffect(() => {
+    const originalOverflow = document.body.style.overflow;
+
     if (isOpen) {
       document.body.style.overflow = 'hidden';
     } else {
-      document.body.style.overflow = '';
+      document.body.style.overflow = originalOverflow;
     }
-
     return () => {
-      document.body.style.overflow = '';
+      document.body.style.overflow = originalOverflow;
     };
   }, [isOpen]);
 
