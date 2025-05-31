4be0f94b5601dd9a2be6ea193d95e822828c59f1
Hyelim Choi <HyelimChoi01@gmail.com>
2025-05-31 18:15:48 +0900

    feat: 사진 추가 기능 작업1

--- src/pages/AddStickerPage.tsx
+++ src/pages/AddStickerPage.tsx
@@ -3,8 +3,41 @@ import { FloatingButton } from '@/components';
 import { PhotoFrame } from '@/components/photo';
 import { usePhotoDownload } from '@/hooks';
 import { useTrackPageView } from '@/service/amplitude/';
+import { useEffect, useRef, useState } from 'react';
+import Moveable from 'react-moveable';
+
+type StickerItem = {
+  id: string;
+  src: string;
+};
+
 const AddStickerPage = () => {
   useTrackPageView({ eventName: '[View] 스티커 추가 페이지' });
+
+  const [addedStickers, setAddedStickers] = useState<StickerItem[]>([]);
+  const [selectedId, setSelectedId] = useState<string | null>(null);
+  const stickerRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
+  const [readyToRenderMoveable, setReadyToRenderMoveable] = useState(false);
+
+  // ✅ 스티커 클릭 시 새로 생성
+  const handleAddSticker = (src: string) => {
+    console.log('?');
+    const id = `sticker-${src}`;
+    setAddedStickers(prev => [...prev, { id, src }]);
+    setSelectedId(id);
+  };
+
+  useEffect(() => {
+    if (selectedId && stickerRefs.current[selectedId]) {
+      console.log(selectedId);
+      setReadyToRenderMoveable(true);
+    } else {
+      setReadyToRenderMoveable(false);
+    }
+  }, [selectedId, addedStickers, setAddedStickers, setSelectedId]);
+
+  const downloadDivRef = useRef<HTMLDivElement>(null);
+  const { handleDownload } = usePhotoDownload(downloadDivRef);
 
   return (
     <div className="flex relative w-full md:h-[92vh] pt-2 pb-2">
@@ -15,6 +48,37 @@ const AddStickerPage = () => {
           className="flex items-center justify-center w-full md:w-[280px] h-fit bg-gray-50 p-4 rounded-lg shadow-md"
         >
           <PhotoFrame />
+          {addedStickers.map(sti => (
+            <div
+              key={sti.id}
+              className="absolute top-0 left-0 z-[100]"
+              ref={el => {
+                stickerRefs.current[sti.id] = el;
+              }}
+              onClick={() => {
+                setSelectedId(sti.id);
+                console.log('sti.id', sti.id);
+              }}
+              style={{ width: 80, height: 80, cursor: 'move' }}
+            >
+              <img src={sti.src} className="w-full h-full" />
+            </div>
+          ))}
+
+          {selectedId && readyToRenderMoveable && (
+            <Moveable
+              key={selectedId}
+              target={
+                stickerRefs.current[selectedId]
+                  ? stickerRefs.current[selectedId]
+                  : console.log('nope')
+              }
+              draggable={true}
+              onDrag={e => {
+                e.target.style.transform = e.transform;
+              }}
+            />
+          )}
         </div>
 
         <section className="flex flex-col w-full h-fit md:h-[85%] p-5 bg-gray-50 rounded-lg shadow-md">
