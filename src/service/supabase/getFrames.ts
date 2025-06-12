4e6f461b5cabab1afc9bdc27487b21890d00ebeb
Hyelim Choi <HyelimChoi01@gmail.com>
2025-06-12 21:09:36 +0900

    feat: 새로운 프레임 전시 우선순위로 변경

--- src/service/supabase/getFrames.ts
+++ src/service/supabase/getFrames.ts
@@ -1,7 +1,11 @@
 import { PRIORITY_FRAMES_TAGS } from '@/constants/framesTag';
 import { getStoragePublicUrl, supabaseClient } from '@/service/supabase';
+import { getCurrentMonthTag } from '@/utils/getCurrentMonthTag';
 
 export const getFrames = async (tags: string[]) => {
+  const currentMonthTag = getCurrentMonthTag();
+  const framePriorityTags = [currentMonthTag, ...PRIORITY_FRAMES_TAGS];
+
   const { data, error } = await supabaseClient
     .from('frames')
     .select('*')
@@ -13,7 +17,7 @@ export const getFrames = async (tags: string[]) => {
 
   return data
     .map(frame => {
-      const index = PRIORITY_FRAMES_TAGS.findIndex(priorityTag =>
+      const index = framePriorityTags.findIndex(priorityTag =>
         frame.tags.includes(priorityTag),
       );
       return {
